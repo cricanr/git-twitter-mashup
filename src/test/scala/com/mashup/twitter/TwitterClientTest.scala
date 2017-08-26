2aec8fa9d41ecc04124cd01443b3d34f7613824c
radu crican <cricanr@gmail.com>
2017-08-27 00:40:53 +0200

    - added test skeleton for twitter client

---
+++ src/test/scala/com/mashup/twitter/TwitterClientTest.scala
@@ -0,0 +1,12 @@
+package com.mashup.twitter
+
+import org.scalatest.{FunSuite, MustMatchers, WordSpec}
+
+class TwitterClientTest extends WordSpec with MustMatchers {
+  "The TwitterClient" when {
+    "" should {
+      "" in {
+      }
+    }
+  }
+}
