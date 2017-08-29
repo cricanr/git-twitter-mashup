3594b3ac2ec46cad643c964cb1e075beb5048368
Radu Crican <cricanr@gmail.com>
2017-08-29 17:41:39 +0200

    - small refactoring

--- src/main/scala/com/mashup/Mashup.scala
+++ src/main/scala/com/mashup/Mashup.scala
@@ -2,40 +2,25 @@ package com.mashup
 
 import com.mashup.RepositoryTweetsSummary._
 import com.mashup.github._
-import com.mashup.twitter.{TweetSummary, TwitterClient, TwitterConfig}
+import com.mashup.twitter.{TweetSummary, TwitterClient, TwitterConfig, TwitterFailure}
 import io.circe.syntax._
-import org.scalactic.{Bad, Good}
-
-import scala.concurrent.ExecutionContext
+import org.scalactic.{Bad, Good, Or}
+import scala.concurrent.{ExecutionContext, Future}
 
 class Mashup(gitHubClient: GitHubClient, twitterClient: TwitterClient) {
   def outputMergeResults(query: String)
-           (implicit gitHubConfig: GitHubConfig,
-            twitterConfig: TwitterConfig,
-            executionContext: ExecutionContext): Unit = {
+                        (implicit gitHubConfig: GitHubConfig,
+                         twitterConfig: TwitterConfig,
+                         executionContext: ExecutionContext): Unit = {
     val reposOrFailureFuture = gitHubClient.getRepositoriesByKeyword(
       gitHubClient.buildRequest(gitHubConfig.endpoint, gitHubConfig.timeout), query)
 
-    reposOrFailureFuture.foreach {
+    reposOrFailureFuture.map {
       case Good(repos) =>
         repos.repositories.map {
           repository =>
-            val tweetsOrFailureFuture = twitterClient.getTweetsByQuery(repository.name, twitterConfig.tweetsToFetchCount, twitterConfig.tweetsUntilDays)
-            tweetsOrFailureFuture.map {
-              case Good(tweetsSummary: List[TweetSummary]) =>
-                val repositoryTweetsSummary = RepositoryTweetsSummary(repository, tweetsSummary)
-                val repositoryTweetsJson = repositoryTweetsSummary.asJson.toString
-                println(s"Repositories containing keyword: $query along with their tweets found are: \n " +
-                  s"$repositoryTweetsJson")
-              case Bad(failure) =>
-                println(s"Failed calling Twitter API to fetch tweets for repository: ${repository.name} " +
-                  s"with failure: ${failure.getClass}")
-
-            }.failed.map {
-              failure =>
-                println("s\"Failed calling Twitter API to fetch tweets for repository: ${repository.name} \" +\n " +
-                  s"with failure: ${failure.getClass}: ${failure.getMessage}")
-            }
+            val tweetsSummaryOrFailureFuture = twitterClient.getTweetsByQuery(repository.name, twitterConfig.tweetsToFetchCount, twitterConfig.tweetsUntilDays)
+            createJson(repository, query, tweetsSummaryOrFailureFuture)
         }
       case Bad(clientGitFailure: ClientGitFailure) =>
         println(s"Client failure calling Git API to fetch repositories containing: $query + \n " +
@@ -44,6 +29,25 @@ class Mashup(gitHubClient: GitHubClient, twitterClient: TwitterClient) {
         println(s"Failed calling Git API to fetch repositories containing: $query +\n " +
           s"with failure: ${genericGitFailure.message}")
       case _ =>
+    }
+  }
+
+  private def createJson(repository: GitHubRepository, query: String,
+                         tweetsSummaryOrFailureFuture: Future[Or[List[TweetSummary], TwitterFailure]])
+                        (implicit executionContext: ExecutionContext) = {
+    tweetsSummaryOrFailureFuture.map {
+      case Good(tweetsSummary: List[TweetSummary]) =>
+        val repositoryTweetsSummary = RepositoryTweetsSummary(repository, tweetsSummary)
+        val repositoryTweetsJson = repositoryTweetsSummary.asJson.toString
+        println(s"Repositories containing keyword: $query along with their tweets found are: \n " +
+          s"$repositoryTweetsJson")
+      case Bad(failure) =>
+        println(s"Failed calling Twitter API to fetch tweets for repository: ${repository.name} " +
+          s"with failure: ${failure.getClass}")
+    }.failed.map {
+      failure =>
+        println("s\"Failed calling Twitter API to fetch tweets for repository: ${repository.name} \" +\n " +
+          s"with failure: ${failure.getClass}: ${failure.getMessage}")
     }
   }
 }
